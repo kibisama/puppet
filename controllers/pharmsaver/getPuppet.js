87ec757031ce54b32c53152ace0b876215f0c24e
unknown <kibisama9@gmail.com>
2025-01-04 18:34:52 -0800

    0.1.0

--- controllers/pharmsaver/getPuppet.js
+++ controllers/pharmsaver/getPuppet.js
@@ -8,7 +8,7 @@ module.exports = async (req, res, next) => {
         break;
       }
     }
-    if (!res.locals.puppetIndex) {
+    if (res.locals.puppetIndex == null) {
       const error = new Error("All Pharmsaver puppets are busy");
       error.status = 503;
       return next(error);
